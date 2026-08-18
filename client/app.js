c6b2268e348f716b0505b23bd22cf8d2f6c5248d
Lakshmi Venkata Sravan Kumar Gudivada <sravangudivada82@gmail.com>
2026-08-18 17:12:58 +0530

    Update app.js

--- client/app.js
+++ client/app.js
@@ -1,4 +1,4 @@
-const API_BASE = "https://anime-recommendation-system-l2u7.vercel.app";
+const API_BASE = "https://anime-recommendation-system-l2u7.vercel.app/api";
 const ITEMS_PER_PAGE = 25;
 
 let currentMode = "title";
